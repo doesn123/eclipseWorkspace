c837508231148d9a4f6e9e6513473d8cd6a46916
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2022-07-26 14:17:39 +0100

    Robs suggestions for the player chooser

--- Wimbledon/src/Main.java
+++ Wimbledon/src/Main.java
@@ -1,33 +1,34 @@
 
 public class Main {
 
-
 	public static void main(String[] args) {
-		Menu m = new Menu();
-		
-		PlayerChooser playerChooser = new PlayerChooser();
-		System.out.println("choose your player...");	
-		TennisPlayer userPlayer = playerChooser.choosePlayer();	
-		System.out.println("choose your opponent...");	
-		TennisPlayer opponent = playerChooser.choosePlayer();	
+		static Scanner scan = new Scanner(System.in);
+		Menu m = new Menu(scan);
+		PlayerChooser playerChooser = new PlayerChooser(scan);
+
+		System.out.println("Wimbledon game");
+		System.out.println("--------------");
+		System.out.println("choose your player...");
+		TennisPlayer userPlayer = playerChooser.selectPlayer();
+		System.out.println("choose your opponent...");
+		TennisPlayer opponent = playerChooser.selectPlayer();
 
 		// TennisPlayer player1 = new TennisPlayer("Novak Djokovic", 35, "male", 71);
 		// TennisPlayer player2 = new TennisPlayer("Nick Kygrios", 26, "male", 65);
 		TennisMatch tennisMatch = new TennisMatch(userPlayer, opponent);
 
-		System.out.println("Wimbledon game");
-		System.out.println("--------------");
+
 		// player1.choosePlayer();
-//		tennisMatch.coinToss();
+		tennisMatch.coinToss();
 		m.mainMenu();
-			
+
 //		System.out.println(t1.chanceOfFirstServeIn);
-		
+
 
 //		Score firstMatch = new Score();
-//		firstMatch.	
-		
-		
+//		firstMatch.
+
+
 //choose player - either t2 or t2?
 	}
 
--- Wimbledon/src/PlayerChooser.java
+++ Wimbledon/src/PlayerChooser.java
@@ -1,50 +1,37 @@
-import java.util.Scanner;
-
 public class PlayerChooser {
 
     private TennisPlayer[] playerList;
-    Scanner scan = new Scanner(System.in);
-    
-//	public PlayerChooser() {
-//        this.playerList = [
-//            new Player("nameA", 22, "Male", 50);
-//            new Player("nameB", 33, "Male", 55);
-//            new Player("nameC", 32, "Male", 23);
-//            new Player("nameD", 25, "Female", 74);
-//            //the above is 'hard coding' - usually want to avoid this
-//        ];
-//    }
+    private Scanner scan;
+
+	public PlayerChooser(Scanner scanner) {
+        this.playerList = [
+            new Player("nameA", 22, "Male", 50);
+            new Player("nameB", 33, "Male", 55);
+            new Player("nameC", 32, "Male", 23);
+            new Player("nameD", 25, "Female", 74);
+        ];
+
+        this.scan = scanner;
+    }
 
 
     public TennisPlayer selectPlayer {
         System.out.println("Choose a player...");
-        
         for(var i = 0; i < this.playerList.length; i++) {
             System.out.println(i + this.playerList[i]);
         }
         char choice = scan.nextLine().charAt(0);
 		switch(choice) {
-            this.playerList[choice];
-
-
-		// case 'a': 
-		// 	System.out.println("You have chosen player " + this.name);
-		// 	break;
-		// case 'b': 
-		// 	System.out.println("You have chosen " + this.name);
-		// 	break;
-					
+            case 'a':
+		 	    System.out.println("You have chosen player " + this.name);
+                // TODO return the player that was selected
+		 	    break;
+		    case 'b':
+		 	    System.out.println("You have chosen " + this.name);
+                 // TODO return the player that was selected
+		 	    break;
+            default:
+                System.out.println("You have chosen a name that is not in the list");
 		}
     }
-    
-	public TennisPlayer choosePlayer() {
-		
-        this.playerList = {
-                           new TennisPlayer("nameA", 22, "Male", 50),
-                           new TennisPlayer("nameB", 33, "Male", 55),
-                           new TennisPlayer("nameC", 32, "Male", 23),
-                           new TennisPlayer("nameD", 25, "Female", 74);
-                           //the above is 'hard coding' - usually want to avoid this
-						  }
-	}
 }
